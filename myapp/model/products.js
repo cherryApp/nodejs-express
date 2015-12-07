ae60c6a097a6c02fb93fa13b34670e181aaa9354
cherryApp <j.cserko@gmail.com>
2015-12-07 13:09:32 +0100

    5. nap

---
+++ myapp/model/products.js
@@ -0,0 +1,100 @@
+// Beemeljük a mongoose modult.
+var mongoose = require('mongoose');
+
+// Csatlakozunk az adatbázishoz.
+try {
+  mongoose.connect('mongodb://localhost/test');  
+} catch ( e ) {
+  console.log( "Error establishing connect to database: ", e );
+}
+
+// Új model beállítása az adatbázis dokumentumokhoz.
+var Product = mongoose.model('Product',
+    {
+        name: "String",
+        price: "Number",
+        manufacturer: "String",
+        image: "String",
+        instock: "Number"
+    });
+
+// Egy dokumentum lekérése.
+var getOne = function( where, callBack ) {
+
+  Product.findOne( where, function( err, docs ) {
+    callBack( docs );
+  } );
+
+};
+
+// Összes dokumentum lekérése.
+var getAll = function( where, callBack ) {
+
+  Product.findOne( where, function( err, docs ) {
+    callBack( docs );
+  } );
+
+};
+
+
+// Dokumentum frissítése.
+var update = function( where, data, callBack ) {
+
+  console.log( 'args', arguments );
+
+  Product.update( where, data, function(err) {
+    if (err) {
+      callBack( {"error": err} );
+    } else {
+      callBack( data );      
+    }
+  });
+
+};
+
+// Új dokumentmum létrehozása.
+var insert = function( data, callBack ) {
+
+  var product = new Product( data );
+  product.save(function (err) {
+    if (err) {
+        callBack( {"error": err} );
+    } else {
+      callBack( data );      
+    }
+  });
+
+};
+
+// Dokumentum törlése.
+var remove = function( where ) {
+  // {age: {$gt:40}
+  User.remove( where, function(err, resp) {
+     if ( !err ) {
+         console.log( "Törölve" );
+     } else {
+         console.log( "Hiba: ", err );
+     }
+  });
+
+};
+
+module.exports = {
+  "getOne": getOne,
+  "getAll": getAll,
+  "update": update,
+  "insert": insert,
+  "remove": remove
+};
+
+
+
+
+
+
+
+
+
+
+
+
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,48 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dashboard' });
+    res.render('index', { title: 'Dashboard' });
+});
+
+/* GET products listing. */
+router.get('/products', function(req, res, next) {
+    var productsHandler = require( '../model/products' );
+    res.send('product page');
+});
+
+/* GET one product. */
+router.get('/products/:id', function(req, res, next) {
+    var productsHandler = require( '../model/products' );
+    productsHandler.getOne( {'_id': req.params.id}, function(data) {
+        res.render('product', { data: data });       
+    } );
+});
+
+/* Update product. */
+router.post('/products/update/:id', function(req, res, next) {
+
+    // console.log( req.body );
+
+    var productsHandler = require( '../model/products' );
+
+    productsHandler.update( {'_id': req.params.id}, req.body, function(data) {
+        res.redirect('/products/'+req.params.id);             
+    } );
+});
+
+/* GET new products. */
+router.get('/products/new', function(req, res, next) {
+    var productsHandler = require( '../model/products' );
+    var product = {
+        name: "Vasaló",
+        price: 12555,
+        manufacturer: "Bosch",
+        image: "http://www.redo.hu/images/stories/virtuemart/product/bosch_vasalo_tda503011p_5177d013e1af4.jpg",
+        instock: 12
+    };
+    productsHandler.insert( product, function(data) {
+        res.send('product inserted: '+data );        
+    } );
 });
 
 module.exports = router;
